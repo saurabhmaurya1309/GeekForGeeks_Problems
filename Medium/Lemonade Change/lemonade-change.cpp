aabaadb97d4471159a73a41d586adb9ea120570b
Saurabh Maurya <118221144+saurabhmaurya1309@users.noreply.github.com>
2023-06-22 10:23:10 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Lemonade Change/lemonade-change.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    bool lemonadeChange(int N, vector<int> &bills) {
+        // code here
+        int ten=0;
+        int five=0;
+        int money=0;
+        for(int i=0;i<N;i++){
+            int x=bills[i];
+            if(x==5){
+                five++;
+                money+=5;
+                continue;
+            }
+           else if(x==10){
+                if(five>0){
+                    five--;
+                    money+=10;
+                    ten++;
+                }
+                else{
+                   
+                    return false;
+                }
+            }
+            else{
+                if(five>0&&ten>0){
+                    five--;
+                    ten--;
+                }
+                else if(five>2){
+                    five-=3;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+
+        vector<int> bills(N);
+        for (int i = 0; i < N; i++) cin >> bills[i];
+
+        Solution obj;
+        int ans = obj.lemonadeChange(N, bills);
+        if (ans)
+            cout << "True" << endl;
+        else
+            cout << "False" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
