4b7a5930d2674442a9ea5a825c43e513f777460b
Saurabh Maurya <118221144+saurabhmaurya1309@users.noreply.github.com>
2023-07-18 15:56:34 +0530

    Added solution - LeetHub

--- Longest Repeating Subsequence - GFG/longest-repeating-subsequence.cpp
+++ Longest Repeating Subsequence - GFG/longest-repeating-subsequence.cpp
@@ -5,30 +5,7 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
 	public:
-	    
-	    int solveTab(string A){
-	        string B=A;
-	        int n=A.length();
-	        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-	       // for(int i=0;i<=n;i++){
-	       //     dp[i][n]=0;
-	       //     dp[n][i]=0;
-	       // }
-	        for(int i=n-1;i>=0;i--){
-	            for(int j=n-1;j>=0;j--){
-	                 if(A[i]==B[j]&&i!=j)
-	                    dp[i][j]=1+dp[i+1][j+1];
-        	       else{
-        	            int ans1=0,ans2=0;
-        	            ans1=dp[i+1][j];
-        	            ans2=dp[i][j+1];
-        	            dp[i][j]= max(ans1,ans2); 
-        	       }
-	            }
-	        }
-	        return dp[0][0];
-	        
-	    }
+	   
 	    int space(string A){
 	        string B=A;
 	         int n=A.length();
@@ -48,7 +25,7 @@ class Solution {
 	            }
 	            next=curr;
 	        }
-	        return curr[0];
+	        return next[0];
 	    }
 		int LongestRepeatingSubsequence(string A){
 		  
