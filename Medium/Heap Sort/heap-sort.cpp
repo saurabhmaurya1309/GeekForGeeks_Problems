531ec3cf0e3fc93b50e1abd12d8ca37430806ec8
Saurabh Maurya <118221144+saurabhmaurya1309@users.noreply.github.com>
2023-07-27 23:10:29 +0530

    Updated Solution - GfG to GitHub

--- Medium/Heap Sort/heap-sort.cpp
+++ Medium/Heap Sort/heap-sort.cpp
@@ -14,7 +14,6 @@ class Solution
     //Heapify function to maintain heap property.
     void heapify(int arr[], int n, int i)  
     {
-      // Your Code Here
       int largest=i;
       int left=2*i+1;
       int right=2*i+2;
@@ -34,7 +33,7 @@ class Solution
     //Function to build a Heap from array.
     void buildHeap(int arr[], int n)  
     { 
-    // Your Code Here
+    // Your Code Here]
         for(int i=n/2-1;i>=0;i--){
             heapify(arr,n,i);
         }
@@ -45,12 +44,12 @@ class Solution
     //Function to sort an array using Heap Sort.
     void heapSort(int arr[], int n)
     {
+        //code here
         buildHeap(arr,n);
         for(int i=n-1;i>=0;i--){
             swap(arr[0],arr[i]);
             heapify(arr,i,0);
         }
-        
     }
 };
 
