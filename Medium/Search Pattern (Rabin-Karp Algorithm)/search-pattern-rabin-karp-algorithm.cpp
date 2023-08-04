3c95bb68d607730ac84c7004cc8e2d2cb63532c7
Saurabh Maurya <118221144+saurabhmaurya1309@users.noreply.github.com>
2023-08-04 11:09:26 +0530

    Updated Solution - GfG to GitHub

--- Medium/Search Pattern (Rabin-Karp Algorithm)/search-pattern-rabin-karp-algorithm.cpp
+++ Medium/Search Pattern (Rabin-Karp Algorithm)/search-pattern-rabin-karp-algorithm.cpp
@@ -7,21 +7,49 @@ using namespace std;
 class Solution
 {
     public:
-        vector <int> search(string pat, string txt)
+    vector<int>res;
+        int d=256;
+        void solve(string pat,string txt, int q)
         {
-            int n=txt.length();
-            int m=pat.length();
-            vector<int>ans;
-            for(int i=0;i<=n-m;i++){
-                string temp=txt.substr(i,m);
-                // cout<<temp<<endl;
-                if(temp==pat){
-                    ans.push_back(i+1);
+            int m=pat.length(),n=txt.length(),p=0,t=0,h=1,j=0;
+            for(int i=0;i<m-1;i++)
+            {
+                h=(h*d)%q;
+            }
+            for(int i=0;i<m;i++)
+            {
+                p=(d*p+pat[i])%q;
+                t=(d*t+txt[i])%q;
+            }
+            for(int i=0;i<=n-m;i++)
+            {
+                if(p==t)
+                {
+                    for(j=0;j<m;j++)
+                    {
+                        if(txt[i+j]!=pat[j])
+                        {
+                            break;
+                        }
+                    }
+                    if(j==m)
+                        res.push_back(i+1);
+                }
+                if(i<n-m)
+                {
+                    t=(d*(t-txt[i]*h)+txt[i+m])%q;
+                    if(t<0)
+                        t=t+q;
                 }
             }
-            if(ans.size()==0)
-                return {-1};
-           return ans;
+        }
+        vector <int> search(string pat, string txt)
+        {
+             int q=INT_MAX;
+            solve(pat,txt,q);
+            if(res.size()==0)
+                res.push_back(-1);
+            return res;
             
         }
      
