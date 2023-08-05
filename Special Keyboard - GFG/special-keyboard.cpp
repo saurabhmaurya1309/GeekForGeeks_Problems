29286172cd3e24481ca76f4f76b1ed682449b744
Saurabh Maurya <118221144+saurabhmaurya1309@users.noreply.github.com>
2023-08-05 12:50:53 +0530

    Added solution - LeetHub

--- Special Keyboard - GFG/special-keyboard.cpp
+++ Special Keyboard - GFG/special-keyboard.cpp
@@ -9,25 +9,18 @@ using namespace std;
 
 class Solution{
 public:
-    long long int solve(int n,vector<int>&dp){
-        if(n<=6){
-            return dp[n]=n;
-        }
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        long long int  ans=0;
-        for(int i=n-3;i>=1;i--){
-            long long int curr=(n-i-1)*solve(i,dp);
-            ans=max(ans,curr);
-           
-        }
-        return dp[n]=ans;
-    }
     long long int optimalKeys(int N){
-            vector<int>dp(N+1,-1);
-          return solve(N,dp);
-      
+        // code here
+          if(N<=6) return N;
+     long long int dp[N+1];
+     dp[0]=0;
+     for(int i=1;i<=6;i++)
+       dp[i]=i;
+       
+   for(int i=7;i<=N;i++)
+       dp[i]=max({2*dp[i-3],3*dp[i-4],4*dp[i-5]});
+       
+   return dp[N];
     }
 };
 
