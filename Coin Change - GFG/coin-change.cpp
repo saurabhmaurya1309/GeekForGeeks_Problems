7b10edcbaa11f983c883f1cb5856104650511e65
Saurabh Maurya <118221144+saurabhmaurya1309@users.noreply.github.com>
2023-08-11 01:57:08 +0530

    Added solution - LeetHub

--- Coin Change - GFG/coin-change.cpp
+++ Coin Change - GFG/coin-change.cpp
@@ -5,23 +5,46 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-   long long int count(int coins[], int N, int sum) {
-        vector<vector<long long int>>dp(N,vector<long long int>(sum+1,0));
-        for(int T=0;T<=sum;T++){
-            dp[0][T]=(T%coins[0]==0);
-        }
-        for(int ind=1;ind<N;ind++){
-            for(int T=0;T<=sum;T++){
-                long long int notTake=dp[ind-1][T];
-                long long int take=0;
-                if(coins[ind]<=T)
-                    take=dp[ind][T-coins[ind]];
-                dp[ind][T]=take+notTake;
-            }
-        }
-        return dp[N-1][sum];
+    
+    long long int solveTab(int coins[],int n,int sum){
+         vector<vector<long long int>>dp(n+1,vector<long long int>(sum+1,0));
+         for(int i=0;i<=n;i++){
+             dp[i][0]=1;
+         }
+         
+         for(int i=0;i<n;i++){
+            //  cout<<"h";
+             for(int j=1;j<=sum;j++){
+                 long long int include=0;
+                //  cout<<j-coins[i]<<endl;
+                 if(j-coins[i]>=0)
+                  include=dp[i][j-coins[i]];
+                //   cout<<include<<endl;
+                 long long int exclude=0;
+                 if(i-1>=0)
+                  exclude=dp[i-1][j];
+                 dp[i][j]=include+exclude;
+             }
+         }
+        //  for(int i=0;i<n;i++){
+        //     //  cout<<"h";
+        //      for(int j=0;j<=sum;j++){
+        //         cout<<dp[i][j]<<" ";
+        //      }cout<<endl;
+        //  }
+         
+         return dp[n-1][sum];
+         
+    }
+       
+    long long int count(int coins[], int N, int sum) {
+
+        
+        vector<vector<long long int>>dp(N+1,vector<long long int>(sum+1,-1));
+        return solveTab(coins,N,sum);
     }
 };
+
 
 //{ Driver Code Starts.
 int main() {
