b2e84253f1e3944b8b65a5b034f2123eba2faa46
Saurabh Maurya <118221144+saurabhmaurya1309@users.noreply.github.com>
2023-10-04 19:25:05 +0530

    Added solution - LeetHub

--- Roman Number to Integer - GFG/roman-number-to-integer.cpp
+++ Roman Number to Integer - GFG/roman-number-to-integer.cpp
@@ -12,77 +12,52 @@ class Solution {
   public:
     int romanToDecimal(string &str) {
         // code here
-          int ans;
-        for(int i=0;i<str.length();i++){
+        int ans=0;
+        int n=str.length();
+        for(int i=0;i<n;i++){
             if(str[i]=='I'){
-                if((i+1<str.length()&&str[i+1]=='V')||(i+1<str.length()&&str[i+1]=='X')){
-                    ans=ans-1;
+                if((i+1<n)&&(str[i+1]=='V'||str[i+1]=='X')){
+                    ans-=1;
+                    // cout<<"sdd"<<endl;
                 }
                 else{
-                    ans=ans+1;
+                    ans+=1;
                 }
             }
-            if(str[i]=='V'){
-                ans=ans+5;
+            else if(str[i]=='V'){
+                // cout<<"df";
+                ans+=5;
             }
-            if(str[i]=='X'){
-                if((i+1<str.length()&&str[i+1]=='C')||(i+1<str.length()&&str[i+1]=='L')){
-                    ans=ans-10;
-                }
-                else{
-                    ans=ans+10;
-                }
-            }
-            if(str[i]=='L'){
-                ans=ans+50;
-            }
-             if(str[i]=='C'){
-                if((i+1<str.length()&&str[i+1]=='D')||(i+1<str.length()&&str[i+1]=='M')){
-                    ans=ans-100;
+            else if(str[i]=='X'){
+                if((i+1<n)&&(str[i+1]=='L'||str[i+1]=='C')){
+                    ans-=10;
                 }
                 else{
-                    ans=ans+100;
+                    ans+=10;
+                }
+                // cout<<ans<<endl;
+            }
+            else if(str[i]=='L'){
+                ans+=50;
+            }
+            else if(str[i]=='C'){
+                if((i+1<n)&&(str[i+1]=='D'||str[i+1]=='M')){
+                    ans-=100;
+                }
+                else{
+                    ans+=100;
                 }
             }
-            if(str[i]=='D'){
-                ans=ans+500;
+            else if(str[i]=='D'){
+                ans+=500;
             }
-            if(str[i]=='M'){
-                ans=ans+1000;
+            else {
+                ans+=1000;
             }
-            
         }
         return ans;
-        
-        
-    //  int value(char roman)
-    // {
-    //     switch(roman)
-    //     {
-    //         case 'I':return 1;
-    //         case 'V':return 5;
-    //         case 'X':return 10;
-    //         case 'L':return 50;
-    //         case 'C':return 100;
-    //         case 'D':return 500;
-    //         case 'M':return 1000;
-    //     }
-    // }
-    // int romanToDecimal(string &str) {
-    //     int i, n, ans=0, p=0;
-    //     n=str.length()-1;
-    //     for( i=n; i>=0; i--)
-    //     {
-    //         if( value(str[i]) >= p)
-    //             ans = ans + value(str[i]);
-    //         else
-    //             ans = ans - value(str[i]);
-    //         p = value(str[i]);
-
-    //     }
-    //     return ans;
     }
-    
+       
 };
 
 
