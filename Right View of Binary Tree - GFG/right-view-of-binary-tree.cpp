bca6aa8cb8414c96e473ac2ac52def2bcffb8c3f
Saurabh Maurya <118221144+saurabhmaurya1309@users.noreply.github.com>
2023-10-29 01:51:43 +0530

    Added solution - LeetHub

--- Right View of Binary Tree - GFG/right-view-of-binary-tree.cpp
+++ Right View of Binary Tree - GFG/right-view-of-binary-tree.cpp
@@ -41,25 +41,34 @@ class Solution
 {
     public:
     //Function to return list containing elements of right view of binary tree.
-   void solve(Node*root,vector<int>&ans,int level)
-    {
-       // Your Code here
-       if(root==NULL){
-        return;
-    }
-    if(level==ans.size()){
-        ans.push_back(root->data);
-    }
-    solve(root->right,ans,level+1);
-    solve(root->left,ans,level+1);
-}
     vector<int> rightView(Node *root)
     {
        // Your Code here
-       vector<int>ans;
-       solve(root,ans,0);
-       return ans;
-    
+         
+           vector<int>ans;
+           if(root==nullptr){
+               return ans;
+           }
+           queue<Node*>q;
+           q.push(root);
+           while(!q.empty()){
+               int n=q.size();
+               for(int i=0;i<n;i++){
+                   Node*frontNode=q.front();
+                   q.pop();
+                   if(i==n-1)
+                   ans.push_back(frontNode->data);
+                   if(frontNode->left){
+                       q.push(frontNode->left);
+                   }
+                   if(frontNode->right){
+                       q.push(frontNode->right);
+                   }
+                   
+               }
+           }
+           return ans;
+   
     }
 };
 
