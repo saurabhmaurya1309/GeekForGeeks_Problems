823baeddc43b9bb37b1bf0920783d5d688afd731
Saurabh Maurya <118221144+saurabhmaurya1309@users.noreply.github.com>
2024-01-12 22:37:41 +0530

    Updated Solution - GfG to GitHub

--- Easy/Reverse First K elements of Queue/reverse-first-k-elements-of-queue.cpp
+++ Easy/Reverse First K elements of Queue/reverse-first-k-elements-of-queue.cpp
@@ -3,39 +3,19 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-queue<int> modifyQueue(queue<int> q, int k);
-int main() {
-    int t;
-    cin >> t;
-    while (t-- > 0) {
-        int n, k;
-        cin >> n >> k;
-        queue<int> q;
-        while (n-- > 0) {
-            int a;
-            cin >> a;
-            q.push(a);
-        }
-        queue<int> ans = modifyQueue(q, k);
-        while (!ans.empty()) {
-            int a = ans.front();
-            ans.pop();
-            cout << a << " ";
-        }
-        cout << endl;
-    }
-}
+
+
 // } Driver Code Ends
-
-
-
-
 // User function Template for C++
 
-// Function to reverse first k elements of a queue.
-queue<int> modifyQueue(queue<int> q, int k) {
-    // add code here.
-    stack<int>s;
+class Solution
+{
+    public:
+    
+    // Function to reverse first k elements of a queue.
+    queue<int> modifyQueue(queue<int> q, int k) {
+        // add code here.
+         stack<int>s;
     for(int i=0;i<k;i++){
         int value=q.front();
         q.pop();
@@ -53,6 +33,32 @@ queue<int> modifyQueue(queue<int> q, int k) {
         q.pop();
     }
     return q;
-    
-    
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t-- > 0) {
+        int n, k;
+        cin >> n >> k;
+        queue<int> q;
+        while (n-- > 0) {
+            int a;
+            cin >> a;
+            q.push(a);
+        }
+        Solution ob;
+        queue<int> ans = ob.modifyQueue(q, k);
+        while (!ans.empty()) {
+            int a = ans.front();
+            ans.pop();
+            cout << a << " ";
+        }
+        cout << endl;
+    }
 }
+// } Driver Code Ends
