4fff8b53c1abfc5ade807e98ddc25c22860fe04a
Saurabh Maurya <118221144+saurabhmaurya1309@users.noreply.github.com>
2024-05-04 01:24:28 +0530

    Updated Solution - GfG to GitHub

--- Basic/Reverse a String/reverse-a-string.cpp
+++ Basic/Reverse a String/reverse-a-string.cpp
@@ -14,18 +14,19 @@ class Solution
     public:
     string reverseWord(string str)
     {
-        // Your code goes here'
-        int i=0;
-        int j=str.length()-1;
-        while(i<j){
-            char ch=str[i];
-            str[i]=str[j];
-            str[j]=ch;
-            i++;
-            j--;
-        }
-        return str;
-        
+        // Your code goes here
+            int i=0,j=str.length()-1;
+            while(i<j){
+                char temp=str[i];
+                // cout<<temp<<endl;;
+                str[i]=str[j];
+                str[j]=temp;
+                i++;
+                j--;
+            }
+            return str;
+            
+            
     }
 };
 
