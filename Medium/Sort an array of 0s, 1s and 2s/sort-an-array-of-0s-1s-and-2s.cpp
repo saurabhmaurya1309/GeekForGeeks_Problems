7201c3496563d9e78e4555794d1ab26d276d5814
Saurabh Maurya <118221144+saurabhmaurya1309@users.noreply.github.com>
2024-05-31 16:36:49 +0530

    Updated Solution - GfG to GitHub

--- Medium/Sort an array of 0s, 1s and 2s/sort-an-array-of-0s-1s-and-2s.cpp
+++ Medium/Sort an array of 0s, 1s and 2s/sort-an-array-of-0s-1s-and-2s.cpp
@@ -11,24 +11,43 @@ class Solution
     void sort012(int a[], int n)
     {
         // code here 
-         int zero=0,one=0,two=0;
-        for(int i=0;i<n;i++){
-            if(a[i]==0)
-                zero++;
-            else if(a[i]==1)
-                one++;
-            else
-                two++;
+        //  int zero=0,one=0,two=0;
+        // for(int i=0;i<n;i++){
+        //     if(a[i]==0)
+        //         zero++;
+        //     else if(a[i]==1)
+        //         one++;
+        //     else
+        //         two++;
+        // }
+        // for(int i=0;i<n;i++){
+        //     if(i<zero)
+        //         a[i]=0;
+        //     else if(i<one+zero)
+        //         a[i]=1;
+        //     else
+        //         a[i]=2;
+        // }
+        
+        int l=0,r=n-1,i=0;
+        while(i<=r){
+            if(a[i]==0){
+                swap(a[l],a[i]);
+                l++;
+                i++;
+            }
+            else if(a[i]==2){
+                swap(a[r],a[i]);
+                r--;
+                
+                
+            }
+            else{
+                i++;
+            }
         }
-        for(int i=0;i<n;i++){
-            if(i<zero)
-                a[i]=0;
-            else if(i<one+zero)
-                a[i]=1;
-            else
-                a[i]=2;
-        }
-    
+        
+
     }
     
 };
