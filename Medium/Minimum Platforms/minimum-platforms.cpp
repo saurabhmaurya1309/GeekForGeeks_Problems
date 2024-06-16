0825e30f54b35f26c7bf876ece5349754914659e
Saurabh Maurya <118221144+saurabhmaurya1309@users.noreply.github.com>
2024-06-17 01:45:20 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Minimum Platforms/minimum-platforms.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+// Program to find minimum number of platforms
+// required on a railway station
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    //Function to find the minimum number of platforms required at the
+    //railway station such that no train waits.
+    int findPlatform(int arr[], int dep[], int n)
+    {
+    	int ans=1; //final value
+    for(int i=0;i<=n-1;i++)
+    {
+        int count=1; // count of overlapping interval of only this   iteration
+        for(int j=0;j<=n-1;j++)
+        {
+            if(i==j){
+                continue;
+            }
+            if(arr[i]>=arr[j] && arr[i]<=dep[j]) 
+           
+           {
+               count++;
+           }
+        }
+        ans=max(ans,count); //updating the value
+    }
+    return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+// Driver code
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--) 
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        int dep[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        for(int j=0;j<n;j++){
+            cin>>dep[j];
+        }
+        Solution ob;
+        cout <<ob.findPlatform(arr, dep, n)<<endl;
+    } 
+   return 0;
+}
+// } Driver Code Ends
