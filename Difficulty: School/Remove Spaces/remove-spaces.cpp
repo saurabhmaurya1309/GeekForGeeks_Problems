df70c83daad1075f9ea288461bc609f46264927d
Saurabh Maurya <118221144+saurabhmaurya1309@users.noreply.github.com>
2024-07-13 16:49:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: School/Remove Spaces/remove-spaces.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    string modify (string s)
+    {
+        //code here.
+        string str;
+        for(int i=0;i<s.length();i++){
+            char ch=s[i];
+            if(ch!=' '){
+                str+=s[i];
+            }
+        }
+        return str;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore ();
+    while (t--)
+    {
+        string s;
+        getline (cin, s);
+        Solution ob;
+        cout <<ob.modify (s) << endl;
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
