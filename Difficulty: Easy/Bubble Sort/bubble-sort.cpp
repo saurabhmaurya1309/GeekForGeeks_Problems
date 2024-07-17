0af8bc6ecd64101631c774810023b7e1e5e8bf1c
Saurabh Maurya <118221144+saurabhmaurya1309@users.noreply.github.com>
2024-07-17 15:30:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Bubble Sort/bubble-sort.cpp
+++ Difficulty: Easy/Bubble Sort/bubble-sort.cpp
@@ -21,12 +21,16 @@ class Solution {
   public:
     // Function to sort the array using bubble sort algorithm.
     void bubbleSort(int arr[], int n) {
-       for(int i=0;i<n;i++){
+       for(int i=0;i<n-1;i++){
+           bool f=true;
            for(int j=0;j<n-i-1;j++){
                if(arr[j]>arr[j+1]){
                    swap(arr[j],arr[j+1]);
+                   f=false;
                }
            }
+           if(f)
+            break;
        }
     }
 };
